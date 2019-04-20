76080a671fb88842dbcee7cfbb73de417e82bcef
pramodith <pballapuram3@gatech.edu.com>
2019-04-20 18:44:57 -0400

    Adding image normalization.

--- DataHandler.py
+++ DataHandler.py
@@ -87,9 +87,9 @@ class DataHandler(Dataset):
             mask = Image.open(self.test_mask_file_names[ind])
             # If all pixels are white in the mask the image does not have any liver cells
             if np.mean(mask)==255:
-                label = 0
-            else:
                 label = 1
+            else:
+                label = 0
             if self.transform is not None:
                 img = self.transform(img)
         return img, label
